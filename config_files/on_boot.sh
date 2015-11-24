659e04b2d28f88199d82a6f2ab1d06afa03706d6
JJ Robertson <jj.robertson@asu.edu>
2015-11-24 23:59:45 +0000

    Added startup script to be run when new Edison is being configured.

--- config_files/on_boot.sh
+++ config_files/on_boot.sh
@@ -3,4 +3,4 @@
 /usr/bin/hciconfig hci0 up
 /usr/bin/hciconfig hci0 piscan
 systemctl start obex
-python /home/root/IOT_Education/config_files/ip.py
+nohup python /home/root/IOT_Education/config_files/ip.py &
