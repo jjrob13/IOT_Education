1fa38be1cd4232f67f4d4f89fa30d990ee1c8023
root <root@edison.mobile.asu.edu>
2015-12-05 22:03:21 +0000

    Clean up object files after startup script builds edison_robot code

--- config_files/startup_script.sh
+++ config_files/startup_script.sh
@@ -6,7 +6,7 @@ src i586 http://iotdk.intel.com/repos/1.1/iotdk/i586'\
 opkg update
 opkg install git
 git clone https://github.com/jjrob13/IOT_Education.git
-(cd IOT_Education; make edison_robot)
+(cd IOT_Education; make edison_robot; make clean)
 chmod +x IOT_Education/config_files/on_boot.sh
 systemctl disable on_boot.service
 cp IOT_Education/config_files/on_boot.service /lib/systemd/system/
