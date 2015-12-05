c238cfeec8646d4fbb63d7d023c6c05b2e6c25f0
JJ Robertson <jj.robertson@asu.edu>
2015-12-05 22:15:36 +0000

    Stop previous startup service in startup script.

--- config_files/startup_script.sh
+++ config_files/startup_script.sh
@@ -9,6 +9,7 @@ git clone https://github.com/jjrob13/IOT_Education.git
 (cd IOT_Education; make edison_robot; make clean)
 chmod +x IOT_Education/config_files/on_boot.sh
 systemctl disable on_boot.service
+systemctl stop on_boot.service
 cp IOT_Education/config_files/on_boot.service /lib/systemd/system/
 systemctl enable on_boot.service
 systemctl start on_boot.service
