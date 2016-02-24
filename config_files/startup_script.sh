ba245d29649d776acb3ff80429801c144bb51fc0
JJ Robertson <jjrob13@gmail.com>
2016-02-24 16:57:33 +0000

    Add ffmpeg install support to startup script.

--- config_files/startup_script.sh
+++ config_files/startup_script.sh
@@ -1,3 +1,4 @@
+#this script is ment to be run from the root directory
 rm -f /etc/opkg/base-feeds.conf
 echo 'src/gz all http://repo.opkg.net/edison/repo/all
 src/gz edison http://repo.opkg.net/edison/repo/edison
@@ -6,8 +7,11 @@ src/gz core2-32 http://repo.opkg.net/edison/repo/core2-32'\
 opkg update
 opkg install git
 opkg install boost boost-dev
+opkg install kernel-module-uvcvideo
 git clone https://github.com/jjrob13/IOT_Education.git
 (cd IOT_Education; make edison_robot; make clean)
+chmod +x IOT_Education/config_files/ffmpeg_files/install_ffmpeg.sh
+./IOT_Education/config_files/ffmpeg_files/install_ffmpeg.sh
 chmod +x IOT_Education/config_files/on_boot.sh
 systemctl disable on_boot.service
 systemctl stop on_boot.service
