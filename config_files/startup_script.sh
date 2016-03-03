45651aec35a62c1f3a5ffa7871751a7aaa39b79b
JJ Robertson <jjrob13@gmail.com>
2016-03-03 16:44:55 +0000

    Add compilation of servo to startup_script.sh

--- config_files/startup_script.sh
+++ config_files/startup_script.sh
@@ -10,6 +10,7 @@ opkg install boost boost-dev
 opkg install kernel-module-uvcvideo
 git clone https://github.com/jjrob13/IOT_Education.git
 (cd IOT_Education; make edison_robot; make clean)
+(cd IOT_Education/servo_example; make calibrate_servo; make clean)
 chmod +x IOT_Education/config_files/ffmpeg_files/install_ffmpeg.sh
 ./IOT_Education/config_files/ffmpeg_files/install_ffmpeg.sh
 chmod +x IOT_Education/config_files/on_boot.sh
